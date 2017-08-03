5e918de4835c0c5a5cab6769fe880f52732f3512
Wong Lok <yellowhappy831@gmail.com>
2017-08-03 10:51:20 +0800

    Initial Commit

---
+++ LokLokARMetal/LokLokARMetal-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  LokLokARMetal-Bridging-Header.h
+//  LokLokARMetal
+//
+//  Created by LOK on 3/8/2017.
+//  Copyright © 2017 WONG LOK. All rights reserved.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
